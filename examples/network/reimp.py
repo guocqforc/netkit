feb3b69a6cb479c2877245d9e59ce008be6a9436
dantezhu <dantezhu@qq.com>
2014-09-01 17:21:12 +0800

    默认用Box

--- examples/network/reimp.py
+++ examples/network/reimp.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
-#from netkit.box import Box
-from netkit.line_box import LineBox as Box
+from netkit.box import Box
+# from netkit.line_box import LineBox as Box
 
 from netkit.log import logger
 import logging
