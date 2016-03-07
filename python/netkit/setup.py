972b5eb17f74f7d2e64b17f3406e55da29084d92
dantezhu <dantezhu@qq.com>
2016-03-07 17:50:39 +0800

    版本升级

--- python/netkit/setup.py
+++ python/netkit/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="netkit",
-    version="0.2.135",
+    version="0.2.136",
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     install_requires=[],
     scripts=[],
